fd784a9af08af78285a2f1758640fd79c4b2ee76
Marcelo Andrade <andrademarcelo98@gmail.com>
2023-01-24 18:02:02 +0000

    fix: dark mode

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -5,14 +5,20 @@ import NoSsr from "@/components/noSsr";
 import { useCodesStore } from "@/store/codes";
 import React from "react";
 
+// TODO:
+/**
+ * EXPORT PDF
+ * DARK MODE iOS
+ */
+
 export default function List() {
   const codes = useCodesStore((state) => state.codes);
 
   return (
     <NoSsr>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-[30px] w-4/5">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Descrição
@@ -24,13 +30,10 @@ export default function List() {
           </thead>
           <tbody>
             {codes.map((c, index) => (
-              <tr
-                key={index}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-              >
+              <tr key={index} className="bg-white border-b ">
                 <th
                   scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
                 >
                   {c.description}
                 </th>
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -20,7 +20,7 @@ export const Input: React.FC<InputProps> = ({
       <div>
         <label
           htmlFor={name}
-          className="block text-sm font-medium text-gray-900 dark:text-white"
+          className="block text-sm font-medium text-gray-900"
         >
           {label}
         </label>
@@ -28,7 +28,7 @@ export const Input: React.FC<InputProps> = ({
           type="text"
           id={name}
           name={name}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           value={value}
           onChange={onChange}
         />
